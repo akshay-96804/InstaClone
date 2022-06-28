83ee540030b5f6eccd749e8c7a97f8e3fa2d1637
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-28 15:22:36 +0530

    minor changes

--- lib/utils/globalvar.dart
+++ lib/utils/globalvar.dart
@@ -1,12 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/screens/add_post_screen.dart';
 import 'package:insta_clone/screens/feed_screen.dart';
+import 'package:insta_clone/screens/profileScreen.dart';
+import 'package:insta_clone/screens/search_screen.dart';
 
 final homeScreenItems = [
-  FeedScreen(),
-  Text(''),
-  AddPostScreen(),
-  Text(''),
-  Text('Profile Screen'),
+  
+  // Text('Profile Screen'),
   // Text(''),
 ];
