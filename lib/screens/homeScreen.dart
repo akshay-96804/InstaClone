6561c1600678696e466f74293552727093faa9fc
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-28 15:18:49 +0530

    minor changes

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -3,6 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/providers/userProvider.dart';
+import 'package:insta_clone/resources/auth_methods.dart';
+import 'package:insta_clone/screens/add_post_screen.dart';
+import 'package:insta_clone/screens/feed_screen.dart';
+import 'package:insta_clone/screens/profileScreen.dart';
+import 'package:insta_clone/screens/search_screen.dart';
 import 'package:insta_clone/utils/colors.dart';
 import 'package:insta_clone/utils/globalvar.dart';
 import 'package:provider/provider.dart';
@@ -16,13 +21,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // String userUid = "" ;
+  
   int _page = 0;
-  late PageController pageController ;
+  late PageController pageController;
 
   @override
   void initState() {
     super.initState();
-    addData() ;
+    addData();
     pageController = PageController();
   }
 
@@ -32,34 +39,43 @@ class _HomeScreenState extends State<HomeScreen> {
     pageController.dispose();
   }
 
-  void navigationTapped(int page){
+  void navigationTapped(int page) {
     pageController.jumpToPage(page);
   }
 
-  void onPageChanged(int page){
+  void onPageChanged(int page) {
     setState(() {
-          _page = page ;
-        });
+      _page = page;
+    });
   }
 
-  addData() async{
-    UserProvider _userProvider = Provider.of<UserProvider>(context,listen: false);
+  addData() async {
+    //  userUid = ;
+    UserProvider _userProvider =
+        Provider.of<UserProvider>(context, listen: false);
     await _userProvider.refreshUser();
+    print(_userProvider.getuser.uid);
   }
 
   @override
   Widget build(BuildContext context) {
-    
     // model.User user = Provider.of<UserProvider>(context).getuser ;
     // print(user);
 
     return Scaffold(
       body: PageView(
-        physics: NeverScrollableScrollPhysics(),
-        onPageChanged: onPageChanged,
-        controller: pageController,
-        children: homeScreenItems
-      ),
+          physics: NeverScrollableScrollPhysics(),
+          onPageChanged: onPageChanged,
+          controller: pageController,
+          children: [
+            FeedScreen(),
+            SearchScreen(),
+            AddPostScreen(),
+            Center(
+              child: Text("Notification Screen"),
+            ),
+            ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid )
+          ]),
       bottomNavigationBar: CupertinoTabBar(
         onTap: navigationTapped,
         backgroundColor: mobileBackgroundColor,
@@ -75,17 +91,17 @@ class _HomeScreenState extends State<HomeScreen> {
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home,
+              icon: Icon(Icons.add,
                   color: _page == 2 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home,
+              icon: Icon(Icons.notifications,
                   color: _page == 3 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home,
+              icon: Icon(Icons.person,
                   color: _page == 4 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
