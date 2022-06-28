bae8209a3f6f3ae1df33f79821ba4b9258612a46
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-28 15:19:07 +0530

    minor changes

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,5 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone/resources/auth_methods.dart';
+import 'package:insta_clone/screens/chatRoom.dart';
+import 'package:insta_clone/screens/chatScreen.dart';
 import 'package:insta_clone/utils/colors.dart';
 import 'package:insta_clone/widgets/post_card.dart';
 
@@ -8,36 +12,75 @@ class FeedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // print(FirebaseAuth.instance.currentUser!.uid);
+
     return Scaffold(
       appBar:  AppBar(
               backgroundColor: mobileBackgroundColor,
               centerTitle: false,
-              title: Text('Instagram',style: TextStyle(color: Colors.white)),
+              title: Text('Photogram',style: TextStyle(color: Colors.white)),
               actions: [
                 IconButton(
                   icon: const Icon(
                     Icons.messenger_outline,
                     color: primaryColor,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    // print("Sign out tapped");
+
+                    Navigator.push(context, MaterialPageRoute(builder: (_){
+                      return ChatsScreen(); 
+                    }));
+                  },
                 ),
               ],
             ),
-         body: StreamBuilder(
-           builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
-             if(snapshot.connectionState == ConnectionState.waiting){
-               return Center(child: CircularProgressIndicator());
-             }
-             return ListView.builder(
-               itemCount: snapshot.data!.docs.length,
-               itemBuilder: (context,index){
-                 return PostCard(
-                   snap : snapshot.data!.docs[index]
-                 );
-             });
-           },
-           stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-         ),   
+            body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if(snapshot.data!.size > 0){
+            return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => Container(
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
+        }
+        return Center(
+          child: Text("No Posts To Display"),
+        );
+          }
+        
+      ),
+        //  body: StreamBuilder(
+        //    builder: (context,AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>> snapshot){
+        //      if(snapshot.hasData){
+        //        return ListView.builder(
+        //        itemCount: snapshot.data!.docs.length,
+        //        itemBuilder: (context,index){
+        //          return PostCard(
+        //            snap : snapshot.data!.docs[index]
+        //          );
+        //       }
+        //        );
+        //      }
+        //     //  if(snapshot.connectionState == ConnectionState.waiting){
+        //     //    return Center(child: CircularProgressIndicator());
+        //     //  }
+        //      return  Center(child: CircularProgressIndicator());
+        //     //  Container(); 
+             
+        //    },
+        //    stream: FirebaseFirestore.instance.collection('posts').orderBy('datePublished',descending: true).snapshots(),
+        //  ),   
     );
   }
 }
