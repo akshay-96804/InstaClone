165269c1b2149e1457ec847393e83fe24c9e9968
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-28 15:20:22 +0530

    firestore functionalities added

--- lib/resources/firestoreMethds.dart
+++ lib/resources/firestoreMethds.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:insta_clone/models/post.dart';
 import 'package:insta_clone/resources/storage_methods.dart';
 import 'package:uuid/uuid.dart';
@@ -35,7 +36,7 @@ class FirestoreMethods {
     return res;
   }
 
-   Future<String> likePost(String postId, String uid, List likes) async {
+  Future<String> likePost(String postId, String uid, List likes) async {
     String res = "Some error occurred";
     try {
       if (likes.contains(uid)) {
@@ -56,4 +57,102 @@ class FirestoreMethods {
     return res;
   }
 
+  Future<void> postComment(String postId, String text, String uid, String name,
+      String profilePic) async {
+    try {
+      if (text.isNotEmpty) {
+        String commentId = const Uuid().v1();
+        await _firebaseFirestore
+            .collection('posts')
+            .doc(postId)
+            .collection('comments')
+            .doc(commentId)
+            .set({
+          'profilePic': profilePic,
+          'name': name,
+          'uid': uid,
+          'text': text,
+          'commentId': commentId,
+          'datePublished': DateTime.now(),
+        });
+      } else {}
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> followUser(String uid, String followId) async {
+    try {
+      DocumentSnapshot snap =
+          await _firebaseFirestore.collection('users').doc(uid).get();
+      List following = (snap.data()! as dynamic)['following'];
+
+      if (following.contains(followId)) {
+        await _firebaseFirestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayRemove([uid])
+        });
+
+        await _firebaseFirestore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayRemove([followId])
+        });
+      } else {
+        await _firebaseFirestore.collection('users').doc(followId).update({
+          'followers': FieldValue.arrayUnion([uid])
+        });
+
+        await _firebaseFirestore.collection('users').doc(uid).update({
+          'following': FieldValue.arrayUnion([followId])
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> createChat(String sender, String reciever, String chatId,String receiverUid) async {
+    await _firebaseFirestore
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('chats')
+        .doc(chatId)
+        .set({
+          'sender': sender, 
+          'reciever': reciever,
+          'recieveruid' : receiverUid
+          });
+
+  await _firebaseFirestore
+        .collection('users')
+        .doc(receiverUid)
+        .collection('chats')
+        .doc(chatId)
+        .set({
+          'sender': reciever, 
+          'reciever': sender,
+          'recieveruid' : FirebaseAuth.instance.currentUser!.uid
+          });      
+
+  }
+
+  Future<void> sendMessage(String sender, String reciever,String message,String chatId,String recieverId) async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('chats')
+        .doc(chatId)
+        .collection('messages').
+        doc(Timestamp.now().toString()).set({
+          'sender': sender, 'message': message
+        });
+
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(recieverId)
+        .collection('chats')
+        .doc(chatId)
+        .collection('messages')
+        .doc(Timestamp.now().toString()).set({
+          'sender': sender, 'message': message
+        });    
+  }
 }
