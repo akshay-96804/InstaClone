b597199c82a6313fe194630a7a56d6f8e4beb751
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-28 15:18:38 +0530

    Authentication added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 import 'package:insta_clone/resources/auth_methods.dart';
 import 'package:insta_clone/screens/homeScreen.dart';
+import 'package:insta_clone/screens/signup_screen.dart';
 import 'package:insta_clone/utils/colors.dart';
 import 'package:insta_clone/utils/utils.dart';
 import 'package:insta_clone/widgets/text_field_input.dart';
@@ -33,11 +34,11 @@ class _LoginScreenState extends State<LoginScreen> {
       _isLoading = true;
     });
 
-    if (res == "Success") {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
-    } else {
-      showSnackBar(res, context);
-    }
+    // if (res == "Success") {
+    //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+    // } else {
+    //   showSnackBar(res, context);
+    // }
     setState(() {
       _isLoading = false;
     });
@@ -51,12 +52,18 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: EdgeInsets.symmetric(horizontal: 32),
           width: double.infinity,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Flexible(child: Container(), flex: 2),
-              // SvgPicture.asset('assets/ic_instagram.svg',
-                  // color: primaryColor, height: 64.0),
-              SizedBox(height: 64.0),
+              
+              // SizedBox(height: 64.0),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text("PhotoGram",style: TextStyle(
+                  letterSpacing: 1.5,
+                  fontSize: 30.0,fontWeight: FontWeight.bold),),
+              ),
+              SizedBox(height: 25.0),
               TextFieldInput(
                   textEditingController: _emailController,
                   hintText: 'Enter email',
@@ -90,21 +97,29 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 12.0),
               Flexible(child: Container(), flex: 2),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: const Text('Dont have an account ?'),
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                  ),
-                  Container(
-                    child: const Text(
-                      'Sign up',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const Text('Dont have an account ?'),
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
                     ),
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                  )
-                ],
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SignUpscreenScreen()));
+                      },
+                      child: Container(
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
